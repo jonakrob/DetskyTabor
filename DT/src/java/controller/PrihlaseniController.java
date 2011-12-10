90fd552eb9c7146576dcffc94a3af1359451e1a3
Robin Jonák <jonakrob@gmail.com>
2011-12-10 21:46:24 +0100

    Refaktorizace PrihlaseniControlleru.

--- DT/src/java/controller/PrihlaseniController.java
+++ DT/src/java/controller/PrihlaseniController.java
@@ -2,7 +2,6 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package controller;
 
 /**
@@ -23,37 +22,27 @@ import org.springframework.web.servlet.view.RedirectView;
 
 public class PrihlaseniController extends SimpleFormController {
 
-
-      @Override
-    public ModelAndView handleRequest(HttpServletRequest hsr,
-                                      HttpServletResponse hsr1) throws Exception {
-        //ModelAndView mv = new ModelAndView("seznamAkci");
-        //ModelAndView mv = new ModelAndView("seznamAkci.htm");
-           ModelAndView mv = new ModelAndView(new RedirectView("seznamAkci.htm"));
-
-            String strDruhAkce = hsr.getParameter("druhAkce");
-            String strIdAkce = hsr.getParameter("idAkce");
-            int druhAkce = Integer.parseInt(strDruhAkce);
-            int idAkce = Integer.parseInt(strIdAkce);
-            HttpSession ses = hsr.getSession();
-            int uzivatelId = 9999;
-            if(ses.getAttribute("userLoggedIn")!=null){
-                uzivatelId = (Integer) ses.getAttribute("userLoggedIn");
-            }
-            UcastNaAkci ucast = new UcastNaAkci();
-            ucast.setAkceId(idAkce);
-            ucast.setDruhAkce(druhAkce);
-            ucast.setUzivatelId(uzivatelId);
-            Session session = HibernateUtil.getSessionFactory().getCurrentSession();
-
-            session.beginTransaction();
-            session.save(ucast);
-
-            session.getTransaction().commit();
-
-            return mv;
-  }
-
+	@Override
+	public ModelAndView handleRequest(HttpServletRequest hsr,
+			HttpServletResponse hsr1) throws Exception {
+		ModelAndView mv = new ModelAndView(new RedirectView("seznamAkci.htm"));
+		String strDruhAkce = hsr.getParameter("druhAkce");
+		String strIdAkce = hsr.getParameter("idAkce");
+		int druhAkce = Integer.parseInt(strDruhAkce);
+		int idAkce = Integer.parseInt(strIdAkce);
+		HttpSession ses = hsr.getSession();
+		int uzivatelId = 9999;
+		if (ses.getAttribute("userLoggedIn") != null) {
+			uzivatelId = (Integer) ses.getAttribute("userLoggedIn");
+		}
+		UcastNaAkci ucast = new UcastNaAkci();
+		ucast.setAkceId(idAkce);
+		ucast.setDruhAkce(druhAkce);
+		ucast.setUzivatelId(uzivatelId);
+		Session session = HibernateUtil.getSessionFactory().getCurrentSession();
+		session.beginTransaction();
+		session.save(ucast);
+		session.getTransaction().commit();
+		return mv;
+	}
 }
-
-
