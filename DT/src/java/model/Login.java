a09ec3236d99cb280b4929d27d310f0b2f55cdf1
Robin Jonák <jonakrob@gmail.com>
2011-12-10 22:01:44 +0100

    Refaktrorizace všech .java modelů.

--- DT/src/java/model/Login.java
+++ DT/src/java/model/Login.java
@@ -2,36 +2,33 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package model;
 
 /**
  *
  * @author Matej
  */
-
-
 public class Login {
-    private String login;
-    private String heslo;
 
-    public String getHeslo() {
-        return heslo;
-    }
+	private String login;
+	private String heslo;
 
-    public void setHeslo(String heslo) {
-        this.heslo = heslo;
-    }
+	public String getHeslo() {
+		return heslo;
+	}
 
-    public String getLogin() {
-        return login;
-    }
+	public void setHeslo(String heslo) {
+		this.heslo = heslo;
+	}
 
-    public void setLogin(String login) {
-        this.login = login;
-    }
+	public String getLogin() {
+		return login;
+	}
 
+	public void setLogin(String login) {
+		this.login = login;
+	}
 
-    public Login(){}
-
+	public Login() {
+	}
 }
